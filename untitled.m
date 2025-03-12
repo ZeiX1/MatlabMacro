ff9d127ba6d0a6606726c156b6a10c11a9375dae
Zeixo <timothee.freyermuth@dauphine.eu>
2025-03-12 15:50:05 +0100

    Added Datasets for Korea and code for data loading

--- untitled.m
+++ untitled.m
@@ -1,23 +1,32 @@
 % Charge csv files as tables
-J1 = readtable('data1.csv');
-J2 = readtable('data2.csv');
-J3 = readtable('data1.csv');
-J4 = readtable('data2.csv');
-J5 = readtable('data1.csv');
-J6 = readtable('data2.csv');
-J7 = readtable('data1.csv');
-J8 = readtable('data2.csv');
-J9 = readtable('data1.csv');
+% NCPHIRSAXDCKRQ    Real Households Final Consumption Expenditure for Republic of Korea,   
+%                   Millions of Domestic Currency, Quarterly, Seasonally Adjusted
+%                   Data Updated: 2024-02-12
+%
+% NGDPRSAXDCKRQ     Real Gross Domestic Product for Republic of Korea, 
+%                   Millions of Domestic Currency, Quarterly, Seasonally Adjusted
+%                   Data Updated: 2025-02-10
+%
+% NFIRNSAXDCKRQ     Real Gross Fixed Capital Formation for Republic of Korea, 
+%                   Millions of Domestic Currency, Quarterly, Not Seasonally Adjusted
+%                   Data Updated: 2025-02-10
+%
+% RTFPNAKRA632NRUG  Total Factor Productivity at Constant National Prices for Republic of  
+%                   Korea, Index 2017=1, Annual, Not Seasonally Adjusted
+%                   Data Updated: 2023-02-27
+%
+% KORCPIALLMINMEI   Consumer Price Indices (CPIs, HICPs), COICOP 1999: Consumer Price      
+%                   Index: Total for Korea, Index 2015=100, Monthly, Not Seasonally Adjusted
+%                   Data Updated: 2023-12-12
+%
+% IRSTCI01KRM156N   Interest Rates: Immediate Rates (< 24 Hours): Call Money/Interbank     
+%                   Rate: Total for Korea, Percent, Monthly, Not Seasonally Adjusted
+%                   Data Updated: 2025-02-17
 
-K1 = readtable('data1.csv');
-K2 = readtable('data2.csv');
-K3 = readtable('data1.csv');
-K4 = readtable('data2.csv');
-K5 = readtable('data1.csv');
-K6 = readtable('data2.csv');
-K7 = readtable('data1.csv');
-K8 = readtable('data2.csv');
-K9 = readtable('data1.csv');
+----------------  ---------------------------------------------------------------------  
+Kor_quaterly = readtable("DataCty\KoreaData\quarterly.csv");
+Kor_monthly = readtable("DataCty\KoreaData\monthly.csv");
+Kor_annual = readtable("DataCty\KoreaData\annual.csv");
 
 % Merge Our datasets
-MergedTable = join(T1, T2, 'Keys', 'ID');
+MergedTableKorea = join(J1,J2,J3,J4,J5,J6,J7,J8,J9, 'Keys', 'ID');
